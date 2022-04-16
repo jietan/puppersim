92615e90bcc081ca6d7997764bf7217b7cb24666
jakegrigsby <jcg6dn@virginia.edu>
2022-04-16 16:15:48 -0400

    reacher ddpg unittests

--- puppersim/reacher/test_ddpg.py
+++ puppersim/reacher/test_ddpg.py
@@ -40,7 +40,7 @@ class DDPG_General(unittest.TestCase):
         )
 
     def test_ddpg_pendulum(self):
-        agent, best_return = ddpg(
+        _, best_return = ddpg(
             self.agent,
             self.train_env,
             self.test_env,
@@ -55,3 +55,54 @@ class DDPG_General(unittest.TestCase):
             verbosity=0,
         )
         self.assertTrue(best_return > -200.0)
+
+
+class DDPG_Reacher(unittest.TestCase):
+    def _setup(self, **env_kwargs):
+        self.train_env = gym.wrappers.TimeLimit(
+            NormActionSpace(ReacherEnv(**env_kwargs)), 500
+        )
+        self.test_env = gym.wrappers.TimeLimit(
+            NormActionSpace(ReacherEnv(**env_kwargs)), 500
+        )
+
+        self.agent = Agent(
+            self.train_env.observation_space.shape[0],
+            self.train_env.action_space.shape[0],
+            hidden_size=256,
+        )
+        self.buffer = ReplayBuffer(
+            10_000,
+            example_state=self.train_env.reset(),
+            example_action=self.train_env.action_space.sample(),
+        )
+
+    def test_ddpg_reacher_position(self):
+        self._setup(run_on_robot=False, render=False, torque_control=False)
+        ddpg(
+            self.agent,
+            self.train_env,
+            self.test_env,
+            self.buffer,
+            num_steps=10,
+            name="test_ddpg_reacher_position",
+            max_episode_steps=500,
+            exploration_anneal=5,
+            render=False,
+            verbosity=0,
+        )
+
+    def test_ddpg_reacher_torque(self):
+        self._setup(run_on_robot=False, render=False, torque_control=True)
+        ddpg(
+            self.agent,
+            self.train_env,
+            self.test_env,
+            self.buffer,
+            num_steps=10,
+            name="test_ddpg_reacher_torque",
+            max_episode_steps=500,
+            exploration_anneal=5,
+            render=False,
+            verbosity=0,
+        )
