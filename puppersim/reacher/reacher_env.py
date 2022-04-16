1031c8ae33a0e4c29cbc8ceef951420dc610ef66
jakegrigsby <jcg6dn@virginia.edu>
2022-04-16 14:22:33 -0400

    torque control draft w ddpg

--- puppersim/reacher/reacher_env.py
+++ puppersim/reacher/reacher_env.py
@@ -11,175 +11,277 @@ from pupper_hardware_interface import interface
 from serial.tools import list_ports
 from sys import platform
 
-KP = 6.0
-KD = 1.0
+KP = 4.0
+KD = 4.0
 MAX_CURRENT = 4.0
 
 
 class ReacherEnv(gym.Env):
+    def __init__(self, run_on_robot=False, render=False, torque_control=True):
+        if torque_control:
+            self._motor_control = pybullet.TORQUE_CONTROL
+            self.action_space = gym.spaces.Box(
+                np.array([-0.1, -0.1, -0.1]),
+                np.array([0.1, 0.1, 0.1]),
+                dtype=np.float32,
+            )
+        else:
+            self._motor_control = pybullet.POSITION_CONTROL
+            self.action_space = gym.spaces.Box(
+                np.array([-0.9 * math.pi, -0.8 * math.pi, -math.pi]),
+                np.array([0.9 * math.pi, 0.8 * math.pi, math.pi]),
+                dtype=np.float32,
+            )
 
-  def __init__(self, run_on_robot=False, render=False):
-    self.action_space = gym.spaces.Box(
-        np.array([-0.9*math.pi, -0.8*math.pi, -math.pi]),
-        np.array([0.9*math.pi, 0.8*math.pi, math.pi]),
-        dtype=np.float32)
-    self.observation_space = gym.spaces.Box(
-        # np.array([-1, -1, -1, -1, -1, -1, 0.05, 0.05, 0.05, -0.3, -0.3, -0.3]),
-        # np.array([1, 1, 1, 1, 1, 1, 0.1, 0.1, 0.1, 0.3, 0.3, 0.3]),
-        # observation space range for target
-        np.array([-0.1, -0.1, 0.05]),
-        np.array([0.1, 0.1, 0.15]),
-        dtype=np.float32)
+        self.observation_space = gym.spaces.Box(
+            np.array(
+                [
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    0.05,
+                    0.05,
+                    0.05,
+                    -0.3,
+                    -0.3,
+                    -0.3,
+                    -float("inf"),
+                    -float("inf"),
+                    -float("inf"),
+                ]
+            ),
+            np.array(
+                [
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                    1,
+                    0.1,
+                    0.1,
+                    0.1,
+                    0.3,
+                    0.3,
+                    0.3,
+                    float("inf"),
+                    float("inf"),
+                    float("inf"),
+                ]
+            ),
+            dtype=np.float32,
+        )
 
-    self._second_arm_position = np.array([0., 0., 0.])
+        self._run_on_robot = run_on_robot
+        if self._run_on_robot:
+            if platform == "linux" or platform == "linux2":
+                serial_port = next(list_ports.grep(".*ttyACM0.*")).device
+            elif platform == "darwin":
+                serial_port = next(list_ports.grep("usbmodem")).device
+            self._hardware_interface = interface.Interface(serial_port)
+            time.sleep(0.25)
+            self._hardware_interface.set_joint_space_parameters(
+                kp=KP, kd=KD, max_current=MAX_CURRENT
+            )
+        else:
+            if render:
+                self._bullet_client = bullet_client.BulletClient(
+                    connection_mode=pybullet.GUI
+                )
+                self._bullet_client.configureDebugVisualizer(
+                    self._bullet_client.COV_ENABLE_GUI, 0
+                )
+                self._bullet_client.resetDebugVisualizerCamera(
+                    cameraDistance=0.3,
+                    cameraYaw=-134,
+                    cameraPitch=-30,
+                    cameraTargetPosition=[0, 0, 0.1],
+                )
+            else:
+                self._bullet_client = bullet_client.BulletClient(
+                    connection_mode=pybullet.DIRECT
+                )
 
-    self.target = np.random.uniform(0.05, 0.1, 3)
+    def reset(self):
+        if self._run_on_robot:
+            return self._get_obs_on_robot()
+        else:
+            self._bullet_client.resetSimulation()
+            URDF_PATH = pd.getDataPath() + "/pupper_arm.urdf"
+            self.robot_id = self._bullet_client.loadURDF(URDF_PATH, useFixedBase=True)
+            self._bullet_client.setGravity(0, 0, -9.8)
+            self.num_joints = self._bullet_client.getNumJoints(self.robot_id)
+            for joint_id in range(self.num_joints):
+                # Disables the default motors in PyBullet.
+                self._bullet_client.setJointMotorControl2(
+                    bodyIndex=self.robot_id,
+                    jointIndex=joint_id,
+                    controlMode=self._bullet_client.POSITION_CONTROL,
+                    targetVelocity=0,
+                    force=0,
+                )
+        # self.target = np.random.uniform(0.05, 0.1, 3)
+        target_angles = np.random.uniform(-0.5 * math.pi, 0.5 * math.pi, 3)
+        self.target = reacher_kinematics.calculate_forward_kinematics_robot(
+            target_angles
+        )
+        # self.target = np.array([0.07, 0.07, 0.07])
+        # target_angles = np.random.uniform(-0.05*math.pi, 0.05*math.pi, 3)
+        # self.target = self._forward_kinematics(target_angles)
 
-    self._run_on_robot = run_on_robot
-    if self._run_on_robot:
-      if platform == "linux" or platform == "linux2":
-        serial_port = next(list_ports.grep(".*ttyACM0.*")).device
-      elif platform == "darwin":
-        serial_port = next(list_ports.grep("usbmodem")).device
-      self._hardware_interface = interface.Interface(serial_port)
-      time.sleep(0.25)
-      self._hardware_interface.set_joint_space_parameters(
-          kp=KP, kd=KD, max_current=MAX_CURRENT)
-    else:
-      if render:
-        self._bullet_client = bullet_client.BulletClient(connection_mode=pybullet.GUI)
-        self._bullet_client.configureDebugVisualizer(self._bullet_client.COV_ENABLE_GUI, 0)
-        self._bullet_client.resetDebugVisualizerCamera(cameraDistance=0.3, cameraYaw=-134, cameraPitch=-30, cameraTargetPosition=[0,0,0.1])
-      else:
-        self._bullet_client = bullet_client.BulletClient(connection_mode=pybullet.DIRECT)
+        self._target_visual_shape = self._bullet_client.createVisualShape(
+            self._bullet_client.GEOM_SPHERE, radius=0.015
+        )
 
-  def reset(self):
-    if self._run_on_robot:
-      return self._get_obs_on_robot()
-    else:
-      self._bullet_client.resetSimulation()
-      URDF_PATH = pd.getDataPath() + "/pupper_arm.urdf"
-      self.robot_id = self._bullet_client.loadURDF(URDF_PATH, useFixedBase=True)
-      self._bullet_client.setGravity(0, 0, -9.8)
-      self.num_joints = self._bullet_client.getNumJoints(self.robot_id)
-      for joint_id in range(self.num_joints):
-        # Disables the default motors in PyBullet.
-        self._bullet_client.setJointMotorControl2(bodyIndex=self.robot_id,
-                                       jointIndex=joint_id,
-                                       controlMode=self._bullet_client.POSITION_CONTROL,
-                                       targetVelocity=0,
-                                       force=0)
+        self._target_visualization = self._bullet_client.createMultiBody(
+            baseVisualShapeIndex=self._target_visual_shape, basePosition=self.target
+        )
 
-    # self.target = np.random.uniform(0.05, 0.10, 3)
-    # Target position: xy range of -0.1 to 0.1. z range of 0.05 to 0.15.
-    self.target = np.concatenate([np.random.uniform(-0.1, 0.1, 2), np.random.uniform(0.05, 0.15, 1)])
-    print("Target: ", self.target)
-    # self.target = np.array([0.07, 0.07, 0.07])
-    # target_angles = np.random.uniform(-0.05*math.pi, 0.05*math.pi, 3)
-    # self.target = self._forward_kinematics(target_angles)
+        return self._get_obs()
 
-    self._target_visual_shape = self._bullet_client.createVisualShape(self._bullet_client.GEOM_SPHERE, radius=0.015)
+    def setTarget(self, target):
+        self.target = target
 
-    self._target_visualization = self._bullet_client.createMultiBody(baseVisualShapeIndex=self._target_visual_shape, basePosition=self.target)
+    def calculateInverseKinematics(self, target_pos):
+        # compute end effector pos in cartesian cords given angles
+        end_effector_link_id = self._get_end_effector_link_id()
+        inverse_kinematics = self._bullet_client.calculateInverseKinematics(
+            self.robot_id, end_effector_link_id, target_pos
+        )
 
-    return self._get_obs()
+        return inverse_kinematics
 
-  def setTarget(self, target):
-    self.target = target
+    def _apply_actions(self, actions):
+        for joint_id, action in zip(range(self.num_joints), actions):
+            joint_velocity = self._bullet_client.getJointState(self.robot_id, joint_id)[
+                1
+            ]
+            joint_pos = self._bullet_client.getJointState(self.robot_id, joint_id)[0]
+            # print("jv: ", joint_velocity)
+            # if joint_id==2:
+            # print("jp: ", joint_pos)
+            # print("act", actions)
+            # Disables the default motors in PyBullet.
+            # print("actions applied: ", actions)
+            if self._motor_control == pybullet.POSITION_CONTROL:
+                self._bullet_client.setJointMotorControl2(
+                    bodyIndex=self.robot_id,
+                    jointIndex=joint_id,
+                    controlMode=self._motor_control,
+                    targetPosition=action,
+                    maxVelocity=1,
+                )
+            else:
+                self._bullet_client.setJointMotorControl2(
+                    bodyIndex=self.robot_id,
+                    jointIndex=joint_id,
+                    controlMode=self._motor_control,
+                    force=action,
+                )
 
-  def calculateInverseKinematics(self, target_pos):
-    # compute end effector pos in cartesian cords given angles
-    end_effector_link_id = self._get_end_effector_link_id()
-    inverse_kinematics = self._bullet_client.calculateInverseKinematics(
-        self.robot_id, end_effector_link_id, target_pos)
+    def _apply_actions_on_robot(self, actions):
+        full_actions = np.zeros([3, 4])
+        full_actions[:, 2] = np.reshape(actions, 3)
 
-    return inverse_kinematics
+        self._hardware_interface.set_joint_space_parameters(
+            kp=KP, kd=KD, max_current=MAX_CURRENT
+        )
+        self._hardware_interface.set_actuator_postions(np.array(full_actions))
 
-  def _apply_actions(self, actions):
-    for joint_id, action in zip(range(self.num_joints), actions):
-      # Disables the default motors in PyBullet.
-      self._bullet_client.setJointMotorControl2(bodyIndex=self.robot_id,
-                                     jointIndex=joint_id,
-                                     controlMode=pybullet.POSITION_CONTROL,
-                                     targetPosition=action)                                
+    def render(self, *args, **kwargs):
+        # here for gym compatability
+        pass
 
-  def _apply_actions_on_robot(self, actions):
-    full_actions = np.zeros([3, 4])
-    full_actions[:, 2] = np.reshape(actions, 3)
+    def _get_obs(self):
+        joint_states = self._bullet_client.getJointStates(
+            self.robot_id, list(range(self.num_joints))
+        )
+        joint_angles = [joint_data[0] for joint_data in joint_states][0:3]
+        joint_velocities = [joint_data[1] for joint_data in joint_states][0:3]
+        # return np.array(self.target)
+        return np.concatenate(
+            [
+                np.cos(joint_angles),
+                np.sin(joint_angles),
+                self.target,
+                self._get_vector_from_end_effector_to_goal(),
+                np.array(joint_velocities) / 20.0,
+            ]
+        )
 
-    self._hardware_interface.set_joint_space_parameters(kp=KP,
-                                                        kd=KD,
-                                                        max_current=MAX_CURRENT)
-    self._hardware_interface.set_actuator_postions(np.array(full_actions))
+    def _get_obs_on_robot(self):
+        self._hardware_interface.read_incoming_data()
+        self._robot_state = self._hardware_interface.robot_state
+        joint_angles = self._robot_state.position[6:9]
+        joint_velocities = self._robot_state.velocity[6:9]
+        np.set_printoptions(precision=2)
+        return np.concatenate(
+            [
+                np.cos(joint_angles),
+                np.sin(joint_angles),
+                self.target,
+                # joint_velocities,
+                self._get_vector_from_end_effector_to_goal(),
+            ]
+        )
 
-  def _get_obs(self):
-    joint_states = self._bullet_client.getJointStates(self.robot_id,
-                                           list(range(self.num_joints)))
-    joint_angles = [joint_data[0] for joint_data in joint_states][0:3]
-    # print("joint angles", joint_angles)
-    joint_velocities = [joint_data[1] for joint_data in joint_states][0:3]
-    # return np.array(self.target)
-    return np.concatenate([
-        # np.cos(joint_angles),
-        # np.sin(joint_angles),
-        self.target,
-        # joint_velocities,
-        # self._get_vector_from_end_effector_to_goal(),
-    ])
+    def step(self, actions):
 
-  def _get_obs_on_robot(self):
-    self._hardware_interface.read_incoming_data()
-    self._robot_state = self._hardware_interface.robot_state
-    joint_angles = self._robot_state.position[6:9]
-    joint_velocities = self._robot_state.velocity[6:9]
-    np.set_printoptions(precision=2)
-    return np.concatenate([
-        # np.cos(joint_angles),
-        # np.sin(joint_angles),
-        self.target,
-        # joint_velocities,
-        # self._get_vector_from_end_effector_to_goal(),
-    ])
+        # print("actions: ", actions)
 
-  def step(self, actions):
+        if self._run_on_robot:
+            self._apply_actions_on_robot(actions)
+            ob = self._get_obs_on_robot()
+        else:
+            self._apply_actions(actions)
+            ob = self._get_obs()
+            self._bullet_client.stepSimulation()
 
-    if self._run_on_robot:
-      self._apply_actions_on_robot(actions)
-      ob = self._get_obs_on_robot()
-    else:
-      self._apply_actions(actions)
-      ob = self._get_obs()
-      self._bullet_client.stepSimulation()
+        torques = []
+        for joint_id, action in zip(range(self.num_joints), actions):
+            if self._motor_control == pybullet.POSITION_CONTROL:
+                torques.append(
+                    self._bullet_client.getJointState(self.robot_id, joint_id)[3]
+                )
+            else:
+                torques.append(action)
+        # print("torques: ", torques)
+        reward_dist = -np.linalg.norm(self._get_vector_from_end_effector_to_goal())
+        # reward_ctrl = -0.1*np.square(torques).sum()
+        reward_ctrl = 0.0  # try without penalty
+        reward = reward_dist + reward_ctrl
 
-    reward_dist = -np.linalg.norm(self._get_vector_from_end_effector_to_goal())
-    reward_ctrl = 0
-    reward = reward_dist + reward_ctrl
+        done = False
+        return ob, reward, done, {}
 
-    done = False
+    def _get_end_effector_link_id(self):
+        for joint_id in range(self.num_joints):
+            joint_name = self._bullet_client.getJointInfo(self.robot_id, joint_id)[1]
+            if joint_name.decode("UTF-8") == "leftFrontToe":
+                return joint_id
+        raise ValueError("leftFrontToe not found")
 
-    return ob, reward, done, {}
+    def _get_vector_from_end_effector_to_goal(self):
+        if self._run_on_robot:
+            joint_angles = self._robot_state.position[6:9]
+            end_effector_pos = reacher_kinematics.calculate_forward_kinematics_robot(
+                joint_angles
+            )
+        else:
+            end_effector_link_id = self._get_end_effector_link_id()
+            end_effector_pos = self._bullet_client.getLinkState(
+                bodyUniqueId=self.robot_id,
+                linkIndex=end_effector_link_id,
+                computeForwardKinematics=1,
+            )[0]
+            # print("end effector: ", end_effector_pos)
+        return np.array(end_effector_pos) - np.array(self.target)
 
-  def _get_end_effector_link_id(self):
-    for joint_id in range(self.num_joints):
-      joint_name = self._bullet_client.getJointInfo(self.robot_id, joint_id)[1]
-      if joint_name.decode("UTF-8") == "leftFrontToe":
-        return joint_id
-    raise ValueError("leftFrontToe not found")
-
-  def _get_vector_from_end_effector_to_goal(self):
-    if self._run_on_robot:
-      joint_angles = self._robot_state.position[6:9]
-      end_effector_pos = reacher_kinematics.calculate_forward_kinematics_robot(joint_angles)
-    else:
-      end_effector_link_id = self._get_end_effector_link_id()
-      end_effector_pos = self._bullet_client.getLinkState(bodyUniqueId=self.robot_id,
-                                               linkIndex=end_effector_link_id,
-                                               computeForwardKinematics=1)[0]
-      # print("end effector: ", end_effector_pos)
-    return np.array(end_effector_pos) - np.array(self.target)
-
-  def shutdown(self):
-    # TODO: Added this function to attempt to gracefully close
-    # the serial connection to the Teensy so that the robot
-    # does not jerk, but it doesn't actually work
-    self._hardware_interface.serial_handle.close()
+    def shutdown(self):
+        # TODO: Added this function to attempt to gracefully close
+        # the serial connection to the Teensy so that the robot
+        # does not jerk, but it doesn't actually work
+        self._hardware_interface.serial_handle.close()
