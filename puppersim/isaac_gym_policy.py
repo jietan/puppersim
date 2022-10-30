7a245363af731ec03535a0b335d1830f8c7d0d32
Wenhao Yu <magicmelon@google.com>
2022-10-29 23:15:09 -0700

    update ig policy deployment script to use the right rp info and settle robot down after exp

--- puppersim/isaac_gym_policy.py
+++ puppersim/isaac_gym_policy.py
@@ -18,7 +18,7 @@ import puppersim
 import os
 
 
-flags.DEFINE_bool("render", True, "Whether to render the example.")
+flags.DEFINE_bool("render", False, "Whether to render the example.")
 flags.DEFINE_bool("profile", False, "Whether to print timing results for different parts of the code.")
 flags.DEFINE_bool("run_on_robot", False, "Whether to run on robot or in simulation.")
 flags.DEFINE_bool("log_to_file", False, "Whether to log data to the disk.")
@@ -82,7 +82,8 @@ class IsaacGymPolicy(object):
     motor_angle = torch.tensor((obs['MotorAngle'] - self.default_dof_pos) * joint_angle_scale) 
     command = torch.tensor(obs['command'] * command_scale)
     last_action = torch.tensor(self.last_action * last_action_scale)
-    new_obs =  torch.cat([rp, rp_dot, motor_angle, command, last_action])
+    new_obs =  torch.cat([rp[[1]], rp[[0]], rp_dot[[1]], rp_dot[[0]], motor_angle, command, last_action])
+    #new_obs = torch.cat([rp, rp_dot, motor_angle, command, last_action])
     
     return torch.tensor(new_obs, device=self.device)
 
@@ -115,7 +116,7 @@ def run_example(num_max_steps=_NUM_STEPS):
   Args:
     num_max_steps: Maximum number of steps this example should run for.
   """
-  path = '/home/pi/model_500.pt'
+  path = '/home/pi/oct_29_model2.pt'
   device = 'cpu'
 
   policy = IsaacGymPolicy(path, device)
@@ -130,6 +131,7 @@ def run_example(num_max_steps=_NUM_STEPS):
   # https://github.com/bulletphysics/bullet3/blob/48dc1c45da685c77d3642545c4851b05fb3a1e8b/examples/pybullet/gym/pybullet_envs/minitaur/robots/quadruped_base.py#L131
   # print("env.action_space=",env.action_space)
   obs = env.reset()
+  input("Press enter to start")
   last_control_step = time.time()
   log_dict = {
       't': [],
@@ -146,7 +148,7 @@ def run_example(num_max_steps=_NUM_STEPS):
       if FLAGS.realtime or FLAGS.run_on_robot:
         # Sync to real time.
         wall_elapsed = time.time() - env_start_wall
-        sim_elapsed = env.env_step_counter * 0.02 # env.env_time_step
+        sim_elapsed = (i+1) * 0.02 # env.env_time_step
         sleep_time = sim_elapsed - wall_elapsed
         if sleep_time > 0:
           time.sleep(sleep_time)
@@ -155,26 +157,15 @@ def run_example(num_max_steps=_NUM_STEPS):
                 f"sim/wall ratio {(sim_elapsed/wall_elapsed):.2f}.")
           last_spammy_log = time.time()
 
-      delta_time = env.robot.GetTimeSinceReset()
-      # 1Hz signal
-      phase = delta_time * 1 * np.pi 
-      # joint angles corresponding to a standing position
-      action = np.array([0, 0.6,-1.2,0, 0.6,-1.2,0, 0.6,-1.2,0, 0.6,-1.2])
-      # modulate the default joint angles by a sinusoid to make the robot do pushups
-      action[:3] = (np.sin(phase) * 0.6 + 0.8) * action[:3]
-      action[9:] = (np.sin(phase) * 0.6 + 0.8) * action[9:]
-      # NOTE: We do not fix the loop rate so be careful if using a policy that is solely dependent on time
-
       before_step_timestamp = time.time()
-      obs['command'] = [0.0, -0.3, 0]
+      obs['command'] = [-0.6, -0.0, 0]
 
       action = policy.step(obs)
-      # print('motor_targets', action)
-      
+      #print('motor_targets', action)
+      #print('motor angles', env.robot.motor_angles) 
       obs, reward, done, _ = env.step(action)
       # print(obs)
       after_step_timestamp = time.time()
-      log_dict['t'].append(delta_time)
       log_dict['IMU'].append(obs['IMU'])
       log_dict['MotorAngle'].append(obs['MotorAngle'])
       log_dict['action'].append(action)
@@ -183,6 +174,7 @@ def run_example(num_max_steps=_NUM_STEPS):
 
       last_control_step = time.time()
   finally:
+    env.robot.set_pose(np.array([ 0.01839886,  1.55314338, -2.69348054, -0.02087028,  1.55497551, -2.69269209, 0.01893148,  1.56903708, -2.68736582, -0.01852664,  1.5660969,  -2.68740849]), duration=3.0)
     if FLAGS.log_to_file:
       print("logging to file...")
       pickle.dump(log_dict, f)
@@ -195,5 +187,3 @@ def main(_):
 
 if __name__ == "__main__":
   app.run(main)
-
-
